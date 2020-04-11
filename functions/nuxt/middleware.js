18c7f3cd410d0131712c5e6de0213dc20717e9c4
vmechele <vincent@myroomabroad.com>
2020-04-11 09:11:59 +0800

    Remove nuxt output folder from git

--- functions/nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../../src/middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['authenticated'] = require('../../src/middleware/authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-export default middleware
--- functions/nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"dark":false,"themes":{"dark":{"primary":"#0099ff","accent":"#717171","secondary":"#CC3300","info":"#808080","warning":"#003366","error":"#FF3333","success":"#00CC33"}}}}
